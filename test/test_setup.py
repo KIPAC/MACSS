1576e1d7d0f8f52ee674695a248351aa503dcc27
Eric Charles <badass@stanford.edu>
2025-10-01 10:50:22 -0600

    Update test_setup.py

--- test/test_setup.py
+++ test/test_setup.py
@@ -1,7 +1,10 @@
-
+import os
 import macss
 
 def test_setup(setup_data: int) -> None:
     assert setup_data == 0
-
+    HOME = os.environ['HOME']
+    data_dir = f"{HOME}/macss"
+    print("The data have been succesfully downloaded and uncompressed, you can find them in {data_dir}")
+    print("You can also ignore any errors from py.test")
     return
